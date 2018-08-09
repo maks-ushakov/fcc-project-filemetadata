96d627cc80d2b44a8a6c067912a7250a31f593c1
Maks <ush.maksim@gmail.com>
2018-08-09 22:39:02 +0300

    Add endpoint for filemetadata

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer();
 
 // require and use "multer"...
 
@@ -14,8 +16,12 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile') ,function(req, res){
+  res.json({
+    'name': req.file.originalname,
+    'type': req.file.mimetype,
+    'size': req.file.size,
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
